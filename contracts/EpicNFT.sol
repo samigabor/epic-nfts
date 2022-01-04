d690e25eb2c79126b7d3489e847b4a3563b69d96
s-gabor <ing.gabor@yahoo.com>
2022-01-04 12:55:02 +0200

    add initial epic nft contract, add run script, remove unused starter files

---
+++ contracts/EpicNFT.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.11;
+
+import "hardhat/console.sol";
+
+contract EpicNFT {
+    constructor() {
+        console.log("This is the Epic NFT contract!");
+    }
+}
