0f4115ec8b09d35c2698489caa9d91727af0269b
s-gabor <ing.gabor@yahoo.com>
2022-01-11 21:00:15 +0200

    implement on-chain metadata

--- README.md
+++ README.md
@@ -1,15 +1,20 @@
-# Basic Sample Hardhat Project
+# Epic Times
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, a sample script that deploys that contract, and an example of a task implementation, which simply lists the available accounts.
+## Depoyed to Rinkeby
 
-Try running some of the following tasks:
+1. 0x2A9e3CC4d5ee5c6C50F9c47ED74a642E1F18a6f2
+2. 0xB806dD81e19Ca6FAE4a5b3914e7442D40448f574
 
-```shell
-npx hardhat accounts
-npx hardhat compile
-npx hardhat clean
-npx hardhat test
-npx hardhat node
-node scripts/sample-script.js
-npx hardhat help
-```
+## Visible on Opeansea
+
+1. https://testnets.opensea.io/collection/epic-v3
+2. https://testnets.opensea.io/collection/epic-v4
+
+## Run project locally
+
+Create a `.env` file at root level and add the following variables:
+
+- `ALCHEMY_API_KEY`
+- `ETHERSCAN_API_KEY`
+- `METAMASK_PRIVATE_KEY`
+
--- contracts/EpicNFT.sol
+++ contracts/EpicNFT.sol
@@ -5,29 +5,22 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "hardhat/console.sol";
 
 contract EpicNFT is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("Epic", "EPC") {
-        console.log("This is the Epic NFT contract!");
-    }
+    constructor() ERC721("Epic", "EPC") {}
 
     function makeAnEpicNFT() public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
-        console.log("make nft %s", tokenId);
         _tokenIdCounter.increment();
         _safeMint(msg.sender, tokenId);
-        _setTokenURI(tokenId, "https://jsonkeeper.com/b/HM46");
-        console.log(
-            "An NFT with id %s was minted on address %s",
+        _setTokenURI(
             tokenId,
-            msg.sender
+            "data:application/json;base64,ewogICAgIm5hbWUiOiAiVWxyYVNvdW5kTW9uZXkiLAogICAgImRlc2NyaXB0aW9uIjogIlRoZSBtb3N0IHNvdW5kIG1vbmV5IG91dCB0aGVyZSIsCiAgICAiaW1hZ2UiOiAiZGF0YTppbWFnZS9zdmcreG1sO2Jhc2U2NCxQSE4yWnlCNGJXeHVjejBpYUhSMGNEb3ZMM2QzZHk1M015NXZjbWN2TWpBd01DOXpkbWNpSUhCeVpYTmxjblpsUVhOd1pXTjBVbUYwYVc4OUluaE5hVzVaVFdsdUlHMWxaWFFpSUhacFpYZENiM2c5SWpBZ01DQXpOVEFnTXpVd0lqNEtJQ0FnSUR4emRIbHNaVDR1WW1GelpTQjdJR1pwYkd3NklIZG9hWFJsT3lCbWIyNTBMV1poYldsc2VUb2djMlZ5YVdZN0lHWnZiblF0YzJsNlpUb2dNVFJ3ZURzZ2ZUd3ZjM1I1YkdVK0NpQWdJQ0E4Y21WamRDQjNhV1IwYUQwaU1UQXdKU0lnYUdWcFoyaDBQU0l4TURBbElpQm1hV3hzUFNKaWJHRmpheUlnTHo0S0lDQWdJRHgwWlhoMElIZzlJalV3SlNJZ2VUMGlOVEFsSWlCamJHRnpjejBpWW1GelpTSWdaRzl0YVc1aGJuUXRZbUZ6Wld4cGJtVTlJbTFwWkdSc1pTSWdkR1Y0ZEMxaGJtTm9iM0k5SW0xcFpHUnNaU0krVld4MGNtRlRiM1Z1WkUxdmJtVjVQQzkwWlhoMFBnbzhMM04yWno0PSIKfQ=="
         );
-        console.log("finished nft %s", tokenId);
     }
 
     ///@dev The following function is override required by Solidity.
