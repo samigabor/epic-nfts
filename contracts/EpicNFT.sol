8c2f709e55025900dcc9d8987417c5763d6fe651
s-gabor <ing.gabor@yahoo.com>
2022-01-13 10:02:14 +0200

    implement dynamic on-chain metadata

--- contracts/EpicNFT.sol
+++ contracts/EpicNFT.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-import "hardhat/console.sol";
+import "./libraries/Base64.sol";
 
 contract EpicNFT is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
@@ -63,18 +63,41 @@ contract EpicNFT is ERC721, ERC721URIStorage, Ownable {
         string memory first = pickRandomFirstWord(tokenId);
         string memory second = pickRandomSecondWord(tokenId);
         string memory third = pickRandomThirdWord(tokenId);
+        string memory combinedWord = string(
+            abi.encodePacked(first, second, third)
+        );
+
         string memory finalSVG = string(
-            abi.encodePacked(svgStartingTag, first, second, third, svgEndingTag)
+            abi.encodePacked(svgStartingTag, combinedWord, svgEndingTag)
         );
 
-        console.log("------------------------\n");
-        console.log(finalSVG);
-        console.log("------------------------\n");
-
-        _setTokenURI(
-            tokenId,
-            "data:application/json;base64,ewogICAgIm5hbWUiOiAiVWxyYVNvdW5kTW9uZXkiLAogICAgImRlc2NyaXB0aW9uIjogIlRoZSBtb3N0IHNvdW5kIG1vbmV5IG91dCB0aGVyZSIsCiAgICAiaW1hZ2UiOiAiZGF0YTppbWFnZS9zdmcreG1sO2Jhc2U2NCxQSE4yWnlCNGJXeHVjejBpYUhSMGNEb3ZMM2QzZHk1M015NXZjbWN2TWpBd01DOXpkbWNpSUhCeVpYTmxjblpsUVhOd1pXTjBVbUYwYVc4OUluaE5hVzVaVFdsdUlHMWxaWFFpSUhacFpYZENiM2c5SWpBZ01DQXpOVEFnTXpVd0lqNEtJQ0FnSUR4emRIbHNaVDR1WW1GelpTQjdJR1pwYkd3NklIZG9hWFJsT3lCbWIyNTBMV1poYldsc2VUb2djMlZ5YVdZN0lHWnZiblF0YzJsNlpUb2dNVFJ3ZURzZ2ZUd3ZjM1I1YkdVK0NpQWdJQ0E4Y21WamRDQjNhV1IwYUQwaU1UQXdKU0lnYUdWcFoyaDBQU0l4TURBbElpQm1hV3hzUFNKaWJHRmpheUlnTHo0S0lDQWdJRHgwWlhoMElIZzlJalV3SlNJZ2VUMGlOVEFsSWlCamJHRnpjejBpWW1GelpTSWdaRzl0YVc1aGJuUXRZbUZ6Wld4cGJtVTlJbTFwWkdSc1pTSWdkR1Y0ZEMxaGJtTm9iM0k5SW0xcFpHUnNaU0krVld4MGNtRlRiM1Z1WkUxdmJtVjVQQzkwWlhoMFBnbzhMM04yWno0PSIKfQ=="
+        /**
+         * json generated:
+         * '{
+         *   "name": "DinamicallyCombinedWord",
+         *   "description": "An epic collection of cool names",
+         *   "image": "data:image/svg+xml;base64,<hashedSVG>"
+         *  }'
+         */
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        combinedWord,
+                        '", "description": "An epic collection of cool names", "image": "data:image/svg+xml;base64,',
+                        Base64.encode(bytes(finalSVG)),
+                        '"}'
+                    )
+                )
+            )
         );
+
+        string memory finalTokenUri = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
+        _setTokenURI(tokenId, finalTokenUri);
     }
 
     function pickRandomFirstWord(uint256 _tokenId)
@@ -110,9 +133,13 @@ contract EpicNFT is ERC721, ERC721URIStorage, Ownable {
     function generateRandomNumber(
         string memory _inputString,
         uint256 _inputNumber
-    ) private pure returns (uint256) {
+    ) private view returns (uint256) {
         string memory input = string(
-            abi.encodePacked(_inputString, Strings.toString(_inputNumber))
+            abi.encodePacked(
+                _inputString,
+                Strings.toString(block.timestamp),
+                Strings.toString(_inputNumber)
+            )
         );
         return uint256(keccak256(abi.encodePacked(input)));
     }
--- hardhat.config.js
+++ hardhat.config.js
@@ -20,7 +20,7 @@ module.exports = {
   solidity: "0.8.11",
   networks: {
     rinkeby: {
-      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [process.env.METAMASK_PRIVATE_KEY],
     },
   },
