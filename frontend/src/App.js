0020a3cadcd43fc954a155555227c8796fd22d7c
s-gabor <ing.gabor@yahoo.com>
2022-01-17 21:04:35 +0200

    remove typescript from frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-import twitterLogo from './assets/twitter-logo.svg';
+import "./App.css";
+import twitterLogo from "./assets/twitter-logo.svg";
 
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = '';
-const TOTAL_MINT_COUNT = 50;
+// const OPENSEA_LINK = "";
+// const TOTAL_MINT_COUNT = 50;
 
 function App() {
   const renderNotConnectedContainer = () => (
