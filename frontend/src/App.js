fd2ecaec688458a9e516e641f7d27902d1624275
s-gabor <ing.gabor@yahoo.com>
2022-01-27 12:49:03 +0200

    FE - add on-chain and off-chain views

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,34 +2,61 @@ import { useEffect, useState } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+import ConnectWallet from "./components/ConnectWallet";
+import Header from "./components/Header";
 import NetworkNotSupported from "./components/NetworkNotSupported";
+import NFTsOnChain from "./components/NFTsOnChain";
 import Footer from "./components/Footer";
-import NFTsOnChainRandomlyGenerated from "./components/NFTsOnChainRandomlyGenerated";
 import { setupEventListener, checkIfWalletIsConnected } from "./shared/web3";
+
+const tabs = {
+  onChain: "On-Chain",
+  offChain: "Off-Chain",
+};
 
 function App() {
   const [account, setAccount] = useState("");
   const [signer, setSigner] = useState(null);
   const [maxSupply, setMaxSupply] = useState(0);
   const [mintedCount, setMintedCount] = useState(0);
-  const [isRinkeby, setIsRinkeby] = useState(false);
+  const [isChainSupported, setIsChainSupported] = useState(false);
+  const [activeTabs, setActiveTabs] = useState(tabs.onChain);
+  const [customData, setCustomData] = useState("");
 
   useEffect(() => {
-    checkIfWalletIsConnected({ setIsRinkeby, setAccount });
+    Object.freeze(tabs); // makes the object act as an enum - no new elements can be added to the object
+    checkIfWalletIsConnected({ setIsChainSupported, setAccount });
     setupEventListener({ setSigner, setMintedCount, setMaxSupply });
   }, []);
 
   return (
     <div className="App">
       <div className="container">
-        {isRinkeby ? (
-          <NFTsOnChainRandomlyGenerated
-            account={account}
-            setAccount={setAccount}
-            signer={signer}
-            mintedCount={mintedCount}
-            maxSupply={maxSupply}
-          />
+        <Header
+          tabs={tabs}
+          activeTabs={activeTabs}
+          setActiveTabs={setActiveTabs}
+        />
+        {isChainSupported ? (
+          <div>
+            {activeTabs === tabs.onChain ? (
+              account !== "" ? (
+                <NFTsOnChain
+                  mintedCount={mintedCount}
+                  maxSupply={maxSupply}
+                  setCustomData={setCustomData}
+                  signer={signer}
+                  customData={customData}
+                />
+              ) : (
+                <div className="metamask-button">
+                  <ConnectWallet setAccount={setAccount} />
+                </div>
+              )
+            ) : (
+              <h2 style={{ color: "white" }}>Comming soon!</h2>
+            )}
+          </div>
         ) : (
           <NetworkNotSupported />
         )}
